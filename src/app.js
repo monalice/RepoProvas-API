382ee35ada55699fdade7ad1f87b042ce0e40dc5
monalice <alice.amorim@engenharia.ufjf.br>
2020-12-16 22:17:20 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+var corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+  methods : '*'
+}
+app.use(cors(corsOptions));
+app.use(express.json());
+
+
+
+module.exports = app;
